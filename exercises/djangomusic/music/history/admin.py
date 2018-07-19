2ff6fa403ccfc35c4a6e6b3accaefdd0b4047c12
rafcevallos <rafael.cevallos81@gmail.com>
2018-07-19 15:52:22 -0500

    Included Admin

--- exercises/djangomusic/music/history/admin.py
+++ exercises/djangomusic/music/history/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from .models import Artist
+from .models import Songs, Artist
 
-# Register your models here.
-admin.site.register(Artist)
+
+class SongsInline(admin.TabularInline):
+    model = Songs
+    extra = 3
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['artist_name']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [SongsInline]
+
+admin.site.register(Artist, ArtistAdmin)
