e158b5b1c77b9249b726d2d1be1f1ab39f4988fe
rafcevallos <rafael.cevallos81@gmail.com>
2018-08-02 14:52:40 -0500

    Created Album and Genre forms.  Updated other models and enabled FKs

--- exercises/djangomusic/music/history/models.py
+++ exercises/djangomusic/music/history/models.py
@@ -1,26 +1,45 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
-
-# Create your models here.
 
 
 class Artist(models.Model):
     artist_name = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    # pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.artist_name
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-
-class Songs(models.Model):
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    album_date = models.DateField('Release Date')
+    label = models.CharField(max_length=30)
+    # songs = models.ForeignKey(Songs, on_delete=models.CASCADE)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    song_text = models.CharField(max_length=200)
-    lyrics = models.CharField(max_length=2000)
 
     def __str__(self):
-        return self.song_text
+        return "{0}".format(self.title)
+
+class Genre(models.Model):
+    style = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "{0}".format(self.style)
+
+class Songs(models.Model):
+    title = models.CharField(max_length=100)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}".format(self.title)
+
+
+
+# class Discography(models.Model):
+#     artist =
+#     albums
